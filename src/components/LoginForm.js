82ff6a160a9ed7b3aac46d2fe9bddc7c08caf4fe
Shin Changseon <skywing27@gmail.com>
2018-11-19 17:06:13 +0900

    feat: ref use example

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,10 +2,16 @@ import React, {Component} from 'react'
 import api from '../api';
 
 export default class LoginForm extends Component {
-
+  constructor(props) {
+    super(props)
+    this.usernameRef = React.createRef()
+    this.passwordRef = React.createRef()
+  }
+  
   handleLogin = async e => {
     e.preventDefault()
-    const username = e.target.elements.username.value
+    // form을 안쓰면서 제어되지 않는 컴포넌트를 쓰고 싶을 때에는 ref를 써야 한다.
+    const username = this.usernameRef.current.value
     const password = e.target.elements.password.value
 
     const res = await api.post('/users/login', {
@@ -17,6 +23,7 @@ export default class LoginForm extends Component {
 
     this.props.onLogin()
   }
+
   render() {
     const {onRegister} = this.props
 
@@ -28,12 +35,14 @@ export default class LoginForm extends Component {
             <input 
               type="text"
               name="username"
+              ref = {this.usernameRef}
             />
           </label>
           <label>비밀번호
             <input 
               type="password"
               name="password"
+              ref = {this.passwordRef}
             />
           </label>
           <button>로그인</button>
