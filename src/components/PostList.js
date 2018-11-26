e7abe1e081a69cb9d39a87dd7332c660e85ef7db
Shin Changseon <skywing27@gmail.com>
2018-11-26 12:42:33 +0900

    style: use scss & BEM

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,38 +1,49 @@
 import React, { Component } from 'react';
 import api from '../api';
 import Layout from './Layout';
+import classNames from 'classnames'
+import './PostList.scss'
 
 export default class PostList extends Component {
   state = {
     posts: [],
-    loading: false
+    loading: true
   }
 
   componentDidMount = async () => {
     const {data: posts} = await api.get('/posts')
     this.setState({
-      posts
+      posts,
+      loading: false
     })
   }
 
   render() {
     const { posts } = this.state
     const { onPostDetailPage, onNewPost, onLoginFormPage } = this.props
-
+    const titleClass = classNames(
+      'PostList__title',
+      {
+        'PostList__title--loading' : this.state.loading
+      }
+    )
     return (
-      <Layout title="게시물 목록" onLoginFormPage = {onLoginFormPage}>
-        <h1>게시물 목록</h1>
-        <ul>
-          {
-            posts.map(post => (
-              <li
-                key={post.id}
-                onClick={() => onPostDetailPage(post.id)}
-              >{post.title}</li>
-            ))
-          }
-        </ul>
-        <button onClick={() => onNewPost()}>새 글 쓰기</button>
+      <Layout title="게시물 목록" onLoginFormPage={onLoginFormPage}>
+        <div className="PostList">
+          <h1 className={titleClass}>게시물 목록</h1>
+          <ul className="PostList__list">
+            {
+              posts.map(post => (
+                <li
+                  className="PostList__item"
+                  key={post.id}
+                  onClick={() => onPostDetailPage(post.id)}
+                >{post.title}</li>
+              ))
+            }
+          </ul>
+          <button onClick={() => onNewPost()}>새 글 쓰기</button>
+        </div>
       </Layout>
     )
   }
